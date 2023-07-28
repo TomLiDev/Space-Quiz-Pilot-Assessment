2c8f51089aa5d6e54483632e07e2ca0372b4aceb
TomLiDev <>
2023-07-28 09:59:37 +0000

    Add styling to quiz questions

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,20 @@
 /**  Overall run game function */
 
-function runQuiz {
+document.addEventListener("DOMContentLoaded", function () {
+    let 
+});
 
+function runQuiz {
 }
 
 function checkAnswer {
-
 }
 
 function calculateCorrectAnswer {
-
 }
 
 function incrementScore {
-
 }
 
 function incrementWrongAnswer {
-
 }
